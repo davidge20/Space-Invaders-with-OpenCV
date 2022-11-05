f17ef54deee6c02e4c23d0efe02dd5a5f4ab5a26
david <dgelody@gmail.com>
2022-11-05 18:55:54 -0400

    commit

--- davidwork.py
+++ davidwork.py
@@ -2,9 +2,11 @@ import math, copy, random
 from cmu_112_graphics import * 
 from PIL import Image
 
-class invader:
-    def __init__(self):
-        pass
+class Invader:
+    def __init__(self, x, y, r):
+        self.x = x
+        self.y = y
+        self.r = r
 
 class bullets:
     def __init__(self, app):
@@ -23,13 +25,14 @@ class enemy():
 
 def appStarted(app):
     #From https://www.tutorialspoint.com/how-to-resize-an-image-using-tkinter
-    image = Image.open("./space.png")
-    spaceResize = image.resize((app.width, app.height))
-    app.backgroundIMG = ImageTk.PhotoImage(spaceResize)
+    app.image1 = Image.open("./space.png")
+    app.spaceResize = app.image1.resize((app.width, app.height))
+    app.backgroundIMG = ImageTk.PhotoImage(app.spaceResize)
 
-    image = Image.open("./spaceship.png")
-    shipResize = image.resize((app.width//7, app.height//7))
-    app.spaceShip = ImageTk.PhotoImage(shipResize)
+    app.image2 = Image.open("./spaceship.png")
+    app.shipResize = app.image2.resize((app.width//7, app.height//7))
+    app.spaceShip = ImageTk.PhotoImage(app.shipResize)
+    
 
     app.width = 400
     app.height = 400
@@ -39,14 +42,30 @@ def appStarted(app):
     app.enemyTime = 0
     app.bulletTime = 0
 
-def keyStarted(app,event):
+    app.player = Invader(app.width//2, y = app.height//2 + app.height//3, r = 10)
+    app.dx = 10
+    app.playerBullets = []
+
+def keyPressed(app,event):
     if event.key == "r":
         app.spaceShip = app.spaceShip.rotate(20)
 
     if event.key == "e":
         app.spaceShip = app.spaceShip.transpose(Image.ROTATE_90)
 
+    if event.key == 'Space':
+        app.bulletX, app.bulletY = app.player.X, app.player.Y
+        app.playerBullets.append((app.bulletX, app.bulletY))
+    if event.key == 'Right':
+        app.player.x += app.dx
+        print(app.player.x)
+    if event.key == 'Left':
+        app.player.x -= app.dx
+
 def timerFired(app):
+    for (app.bulletX, app.bulletY) in app.playerBullets:
+        app.bulletY -= 10
+
     app.enemyTime += 100
     if app.enemyTime == 1000 and len(app.enemyList) < 7:
          currentEnemy = enemy(app)
@@ -92,7 +111,13 @@ def redrawAll(app,canvas):
     canvas.create_image(200, 300, image = app.spaceShip)
 
     exampleEnemy(app,canvas)
-    exampleBullet(app, canvas)    
+    exampleBullet(app, canvas) 
+
+    canvas.create_oval(app.player.x - app.player.r, app.player.y - app.player.r, 
+                     app.player.x + app.player.r, app.player.y +app.player.r)
+    for (app.bulletX, app.bulletY) in app.playerBullets:
+        canvas.create_oval(app.bulletX - app.player.r, app.bulletY - app.player.r,
+                           app.bulletX + app.player.r, app.bulletY + app.player.r)   
 
 def main():
     runApp(width = 400, height = 400)
