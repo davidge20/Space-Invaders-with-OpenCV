925d15bcca894f5a6b704aa1a03f34aef3b2a789
david <dgelody@gmail.com>
2022-11-05 15:10:33 -0400

    changes

--- mainwork.py
+++ mainwork.py
@@ -1,5 +1,21 @@
+import math, copy, random
+
 print("hello world")
 
 class invader:
     def __init__(self):
         self.r = 20
+
+def main():
+    app = invader
+
+if __name__ == "__main__":
+    main()
+
+def appStarted():
+    pass
+
+def keyStarted(app,event):
+    pass
+
+# runApp(width = 400, height = 400)
