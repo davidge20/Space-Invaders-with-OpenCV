ff19eb20309bd4742f6988f05f2906b1f7c4f2a1
david <dgelody@gmail.com>
2022-11-05 16:47:25 -0400

    davidschanges

--- davidwork.py
+++ davidwork.py
@@ -7,6 +7,14 @@ class invader:
         # self.x = app.width//2
         pass
 
+class bullets:
+    def __init__(self, app):
+        for enemy in app.enemyList:
+            self.x = enemy.getXY[0]
+            self.y = enemy.getXY[1]
+            app.enemyDict[self.x].get()
+
+
 class enemy():
     def __init__(self, app):
         self.x = random.randrange(app.width//6, app.width * 5//6, 40)
@@ -25,6 +33,7 @@ def appStarted(app):
     app.width = 400
     app.height = 400
     app.enemyList = []
+    app.enemyDict = dict()
     app.enemyXSet = set()
     app.time = 0
 
@@ -36,14 +45,22 @@ def keyStarted(app,event):
 def timerFired(app):
     app.time += 100
     if app.time == 1000:
+         x = enemy(app).getXY()[0]
+         y = enemy(app).getXY()[1]
          app.enemyList = app.enemyList + [(enemy(app).getXY())]
+         app.enemyDict[x] = y
          print(app.enemyList)
+         print(app.enemyDict)
          app.time = 0
 
-    for coords in app.enemyList:
-        yCord = coords[1]
-        yCord += 1
-        coords[1] = yCord
+    for cords in app.enemyList:
+        print(cords)
+        cords[1] += 1
+        app.enemyDict.get(cords[0],0) + 1
+        print(app.enemyDict)
+
+def exampleBullet(app,canvas):
+    pass
 
 def exampleEnemy(app,canvas):
     for enemy in app.enemyList:
