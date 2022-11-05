a4d5623230f88f198b35496b63c7018e27c726d9
car01rac <114450347+car01rac@users.noreply.github.com>
2022-11-05 19:57:41 -0400

    yyhyhyh

--- carolynWork.py
+++ carolynWork.py
@@ -12,6 +12,7 @@ def appStarted(app):
     app.lives = 3
     app.score = 0
     app.gameOver = False
+    #app.enemyList
     #app.bulets
     #app.spaceShip = 
 
@@ -25,11 +26,44 @@ def dotIntersectsPaddle(app):
             (app.paddleY0 <= app.dotCy <= app.paddleY1))
 
 def checkInvaderCollision(app):
-    app.spa
+    app.I
     pass
 
-def checkEnemyCollision(app):
-    pass
+#need informantion on how invader bulets are stored first
+# def checkEnemyCollision(app):
+#     for enemy in app.enemyList:
+#         cx = enemy[0]
+#         cy = enemy[1]
+#         r = 10
+#         x0 = cx - r
+#         y0 = cy - r
+#         x1 = cx + r
+#         y1 = cy + r
+#         for bulet in app.enemyDict
+#     return ()
+
+#app.enemyDict[bullet] += 5
+def checkInvaderCollison(app):
+    #once the actual invader is made these values would be initialized in app.
+    invaderCX = 10
+    invaderCY = 10
+    invaderWidth = 5
+    invaderHeight = 5
+    #assuming the invader is a rectagle
+    invaderX0 = invaderCX - (invaderWidth / 2)
+    invaderX1 = invaderCX + (invaderWidth / 2)
+    invaderY0 = invaderCY - (invaderHeight / 2)
+    invaderY1 = invaderCY + (invaderHeight / 2)
+    #bulet = current x
+    #app.enemydict[bulet] = current y
+    buletX0 = bulet - r
+    buletX1 = bulet + r
+    buletY0 = app.enemydict[bulet] - r
+    buletY1 = app.enemydict[bulet] + r
+    return ((invaderX0 <= buletX0 <= invaderX1) or 
+            (invaderX0 <= buletX1 <= invaderX1) or 
+            (invaderY0 <= buletY0 <= invaderY1) or 
+            (invaderY0 <= buletY1 <= invaderY1))
 
 def drawBorder(app, canvas):
     canvas.create_rectangle(0, 0, app.width, app.height/10, fill = 'green')
@@ -57,13 +91,28 @@ def drawIntroduction(app, canvas):
                         text = 'Press r to start', font = 'courier 10',
                         fill = 'green')
 
+def drawEnemy(app, canvas, cx, cy, r):
+    #body
+    canvas.create_oval(cx - r * (4 / 3), cy - r, cx + r * (4 / 3), cy + r,
+                     fill = 'green')
+    #left eye
+    canvas.create_oval(cx - r * (5 / 6), cy - r * (4 / 5),
+                             cx - r * (1 / 6), cy - r * (1 / 5), fill = 'black')
+    # rigth eye
+    canvas.create_oval(cx + r * (1 / 6), cy - r * (4 / 5),
+                            cx + r * (5 / 6), cy - r * (1 / 5), fill = 'black')
+
+def drawInvader(app, canvas, cx, cy, r):
+    pass
+
 def keyStarted(app,event):
     pass
 
 def redrawAll(app,canvas):
-    drawBorder(app, canvas)
+    #drawBorder(app, canvas)
     #drawGameOver(app, canvas)
-    drawIntroduction(app, canvas)
+    #drawIntroduction(app, canvas)
+    drawEnemy(app, canvas, 200, 200, 100)
     
 
 def main():
