1d564b378a0060e63bb1ca2fe3a6572937697585
Adrian <adrianm2003az@gmail.com>
2022-11-05 18:43:39 -0400

    change

--- adrianwork.py
+++ adrianwork.py
@@ -1,33 +1,42 @@
 import math, copy, random
 from cmu_112_graphics import *
+#from cmu_cs3_graphics import *
 
 class Invader:
     def __init__(self, x, y, r):
         self.x = x
         self.y = y
         self.r = r
-    def getX(self):
-        return self.x
-    def getY(self):
-        return self.y
-    def getR(self):
-        return self.r
 
-def onAppStart(app):
+def appStarted(app):
     x = app.width//2
     y = app.height//2 + app.height//3
     r = 10
     app.player = Invader(x, y, r)
     app.dx = 10
+    app.playerBullets = []
+   
 
-def onKeyHold(app, keys):
-    if ('right' in keys):
+def keyPressed(app, event):
+    if event.key == 'Space':
+        app.bulletX, app.bulletY = app.player.X, app.player.Y
+        app.playerBullets.append((app.bulletX, app.bulletY))
+    if event.key == 'Right':
         app.player.x += app.dx
-    if ('left' in keys):
+        print(app.player.x)
+    if event.key == 'Left':
         app.player.x -= app.dx
 
+def timerFired(app):
+    for (app.bulletX, app.bulletY) in app.playerBullets:
+        app.bulletY -= 10
+
 def redrawAll(app, canvas):
-    canvas.create_oval(app.player.getX(), app.player.getY(), app.player.getR())
+    canvas.create_oval(app.player.x - app.player.r, app.player.y - app.player.r, 
+                     app.player.x + app.player.r, app.player.y +app.player.r)
+    for (app.bulletX, app.bulletY) in app.playerBullets:
+        canvas.create_oval(app.bulletX - app.player.r, app.bulletY - app.player.r,
+                           app.bulletX + app.player.r, app.bulletY + app.player.r)
 
 def main():
     runApp()
