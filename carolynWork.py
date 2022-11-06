c2714b6f2283a487fc4841f48f19612f2fe29bfc
car01rac <114450347+car01rac@users.noreply.github.com>
2022-11-05 21:27:31 -0400

    enemy collison

--- carolynWork.py
+++ carolynWork.py
@@ -25,45 +25,38 @@ def dotIntersectsPaddle(app):
     return ((app.paddleX0 <= app.dotCx <= app.paddleX1) and
             (app.paddleY0 <= app.dotCy <= app.paddleY1))
 
-def checkInvaderCollision(app):
-    app.I
-    pass
-
-#need informantion on how invader bulets are stored first
-# def checkEnemyCollision(app):
-#     for enemy in app.enemyList:
-#         cx = enemy[0]
-#         cy = enemy[1]
-#         r = 10
-#         x0 = cx - r
-#         y0 = cy - r
-#         x1 = cx + r
-#         y1 = cy + r
-#         for bulet in app.enemyDict
-#     return ()
-
-#app.enemyDict[bullet] += 5
-def checkInvaderCollison(app):
-    #once the actual invader is made these values would be initialized in app.
-    invaderCX = 10
-    invaderCY = 10
-    invaderWidth = 5
-    invaderHeight = 5
-    #assuming the invader is a rectagle
-    invaderX0 = invaderCX - (invaderWidth / 2)
-    invaderX1 = invaderCX + (invaderWidth / 2)
-    invaderY0 = invaderCY - (invaderHeight / 2)
-    invaderY1 = invaderCY + (invaderHeight / 2)
+def checkInvaderCollison(app, bullet):
+    r = 10
+    invaderX0 = app.player.x - app.player.r
+    invaderX1 = app.player.x + app.player.r
+    invaderY0 = app.player.y - app.player.r
+    invaderY1 = app.player.y + app.player.r
     #bulet = current x
     #app.enemydict[bulet] = current y
-    buletX0 = bulet - r
-    buletX1 = bulet + r
-    buletY0 = app.enemydict[bulet] - r
-    buletY1 = app.enemydict[bulet] + r
-    return ((invaderX0 <= buletX0 <= invaderX1) or 
-            (invaderX0 <= buletX1 <= invaderX1) or 
-            (invaderY0 <= buletY0 <= invaderY1) or 
-            (invaderY0 <= buletY1 <= invaderY1))
+    bulletX0 = bullet - r
+    bulletX1 = bullet + r
+    bulletY0 = app.enemydict[bullet] - r
+    bulletY1 = app.enemydict[bullet] + r
+    return ((invaderX0 <= bulletX0 <= invaderX1) or 
+            (invaderX0 <= bulletX1 <= invaderX1) or 
+            (invaderY0 <= bulletY0 <= invaderY1) or 
+            (invaderY0 <= bulletY1 <= invaderY1))
+
+def checkEnemyCollision():
+    r = 10
+    bulletX0 = bullet[0] - r
+    bulletX1 = bullet[0] + r
+    bulletY0 = bullet[1] - r
+    bulletY1 = bullet[1] + r
+    for enemy in app.enemyList:
+        enemyX0 = enemy[0] - r
+        enemyX1 = enemy[0] + r
+        enemyY0 = enemy[1] - r
+        enemyY1 = enemy[1] + r
+        return (((enemyX0 <= bulletX0 <= enemyX1) or 
+                (enemyX0 <= bulletX1 <= enemyX1)) and 
+                ((enemyY0 <= bulletY0 <= enemyY1) or
+                (enemyY0 <= bulletY1 <= enemyY1)))
 
 def drawBorder(app, canvas):
     canvas.create_rectangle(0, 0, app.width, app.height/10, fill = 'green')
@@ -101,6 +94,11 @@ def drawEnemy(app, canvas, cx, cy, r):
     # rigth eye
     canvas.create_oval(cx + r * (1 / 6), cy - r * (4 / 5),
                             cx + r * (5 / 6), cy - r * (1 / 5), fill = 'black')
+    # left antenna 
+    canvas.create_rectangle(cx - r * (1 / 2), cy - r, cx + r * (1 / 2), cy + r,
+                            fill = 'green')
+    # right antenna
+    canvas.crete_rectangle()
 
 def drawInvader(app, canvas, cx, cy, r):
     pass
