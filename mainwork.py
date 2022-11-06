a1e08ce42957d969a807bcf960f21b2f72db600a
car01rac <114450347+car01rac@users.noreply.github.com>
2022-11-05 20:13:07 -0400

    checking collision

--- mainwork.py
+++ mainwork.py
@@ -97,6 +97,7 @@ def timerFired(app):
                     app.enemyDict[bullet] = cords[1]
             continue
         app.enemyDict[bullet] += 5
+        checkInvaderCollision(app, bullet)
 
 def exampleBullet(app,canvas):
     for bullet in app.enemyDict:
@@ -110,7 +111,24 @@ def exampleEnemy(app,canvas):
         x = enemy[0]
         y = enemy[1]
         r = 15
-        canvas.create_oval(x - r, y - r, x + r, y + r, fill = "blue")   
+        canvas.create_oval(x - r, y - r, x + r, y + r, fill = "blue")  
+
+def checkInvaderCollison(app, bullet):
+    r = 10
+    invaderX0 = app.player.x - app.player.r
+    invaderX1 = app.player.x + app.player.r
+    invaderY0 = app.player.y - app.player.r
+    invaderY1 = app.player.y + app.player.r
+    #bulet = current x
+    #app.enemydict[bulet] = current y
+    buletX0 = bullet - r
+    buletX1 = bullet + r
+    buletY0 = app.enemydict[bullet] - r
+    buletY1 = app.enemydict[bullet] + r
+    return ((invaderX0 <= buletX0 <= invaderX1) or 
+            (invaderX0 <= buletX1 <= invaderX1) or 
+            (invaderY0 <= buletY0 <= invaderY1) or 
+            (invaderY0 <= buletY1 <= invaderY1)) 
 
 def drawBorder(app, canvas):
     canvas.create_rectangle(0, 0, app.width, app.height/10, fill = 'green')
