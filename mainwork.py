775e1ec44b310891ebfaea10dc771e45f3d86e6c
Tsairus Beasley <tsairus10@gmail.com>
2022-11-05 20:04:37 -0400

    eye

--- mainwork.py
+++ mainwork.py
@@ -2,6 +2,36 @@ import math, copy, random
 from cmu_112_graphics import * 
 from PIL import Image
 
+import cv2
+
+###############################################################################
+#https://itsourcecode.com/free-projects/opencv/eye-detection-opencv-python-with-source-code/
+
+# Load the cascade
+face_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+# To capture video from webcam. 
+cap = cv2.VideoCapture(0)
+while True:
+    # Read the frame
+    _, img = cap.read()
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Detect the faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    print(faces)
+    # Draw the rectangle around each face
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    # Display
+    cv2.imshow('img', img)
+    # Stop if escape key is pressed
+    k = cv2.waitKey(30) & 0xff
+    if k==27:
+        break
+# Release the VideoCapture object
+cap.release()
+
+###################################################################
 class Invader:
     def __init__(self, x, y, r):
         self.x = x
